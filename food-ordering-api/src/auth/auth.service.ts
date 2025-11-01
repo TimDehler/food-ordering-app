577aaa878ce76aaf446661313d953b1a7f8137ea
TimDehler <tim.dehler@tibidi.de>
2025-11-01 15:22:29 +0100

    fixing docker compose and bcrypt package mismatch

--- food-ordering-api/src/auth/auth.service.ts
+++ food-ordering-api/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 import { User, UserRole } from '@prisma/client';
 import { UsersService } from '../users/users.service';
